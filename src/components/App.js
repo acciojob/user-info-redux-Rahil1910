2bde3f1cc8a52d0f07dbe76437a2e2d5eb34491e
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-17 16:38:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Display from "./displayData";
 const App = () => {
   return (
     <div>
+        <h1>User Information</h1>
         <InputForm/>
         <p>Current values in store:</p>
         <Display/>

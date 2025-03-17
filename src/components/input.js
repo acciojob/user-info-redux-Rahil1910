f05eac826005bc25d9d4116f0f28eaad928f36d2
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-17 16:25:49 +0530

    Create input.js

---
+++ src/components/input.js
@@ -0,0 +1,45 @@
+import {useDispatch} from "react-redux";
+import { useState } from "react";
+import { UpdateValues } from "./actions";
+
+const InputForm =()=>{
+    const [data,setData] = useState({
+        name:"",
+        email:""
+    })
+    const dispatch = useDispatch();
+
+    const inputText = (e) =>{
+        const {name,value} = e.target;
+        setData((prev) => {
+            const updatedData = { ...prev, [name]: value };
+            dispatch(UpdateValues(updatedData.name, updatedData.email));
+            return updatedData;
+          });
+    }
+    return(
+        <>
+        <form>
+            <label>Name:</label>
+            <input
+                type="text"
+                name="name"
+                value={data.name}
+                placeholder="enter name"
+                onInput={inputText}
+            />
+            <br/>
+            <label>Email:</label>
+            <input
+                type="text"
+                name="email"
+                value={data.email}
+                placeholder="enter email"
+                onInput={inputText}
+            />
+        </form>
+        </>
+    )
+}
+
+export default InputForm;

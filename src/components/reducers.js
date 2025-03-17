6043cbd659a4b098d102adf4c852bb72a2484327
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-17 16:26:35 +0530

    Create reducers.js

---
+++ src/components/reducers.js
@@ -0,0 +1,11 @@
+const initialState = {name:"",email:""};
+
+const Reducer = (state = initialState,action) =>{
+    switch(action.type){
+        case "input" : 
+            return {...state, ...action.payload};
+        default:
+            return state;
+    }
+}
+export default Reducer;

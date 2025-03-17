188eba91f2397ce976954c46f5981fe3fbb91c15
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-17 17:09:16 +0530

    Update displayData.js

--- src/components/displayData.js
+++ src/components/displayData.js
@@ -1,6 +1,6 @@
 import {useSelector} from "react-redux";
 import React from "react";
-import styles from "./display.module.css";
+import "./display.css";
 
 const Display = () =>{
     const {name,email} = useSelector((state)=>state);
@@ -8,10 +8,10 @@ const Display = () =>{
         <>
         <div>
             <label>Name - </label>
-            <p className={styles.output}>{name}</p>
+            <p className="output">{name}</p>
             <br></br>
             <label>Email - </label>
-            <p className={styles.output}>{email}</p>
+            <p className="output">{email}</p>
         </div>
         </>
     )

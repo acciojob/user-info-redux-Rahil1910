b2372ba0d909f2950e2cf21fa8789934a417e588
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-17 16:24:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
-import React from "react";
 import './../styles/App.css';
+import React from "react";
+import InputForm from "./input";
+import Display from "./displayData";
+
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <InputForm/>
+        <p>Current values in store:</p>
+        <Display/>
     </div>
   )
 }
 
 export default App
+
